94eb7f51988951a9317a7ba16e75e052788a7d67
Alexander Gavrilov <angavrilov@gmail.com>
2015-07-09 21:27:18 +0300

    Change physics object pinner to use a full panel.
    Unpinning doesn't really work for some reason.

--- physics_pin_select.py
+++ physics_pin_select.py
@@ -20,20 +20,25 @@ class SPACE_OT_pin_physics_object(bpy.types.Operator):
     @classmethod
     def poll(cls, context):
         objname = context.scene.physics_pin_object
-        return context.space_data.use_pin_id and objname != "" and objname in bpy.data.objects
+        return context.space_data.type == 'PROPERTIES' and objname != "" and objname in bpy.data.objects
 
     def execute(self, context):
         context.space_data.pin_id = bpy.data.objects[context.scene.physics_pin_object]
+        context.space_data.use_pin_id = True
         return {'FINISHED'}
 
-def draw_pin_selector(self, context):
-    layout = self.layout
-    space = context.space_data
+class SPACE_OT_unpin_object(bpy.types.Operator):
+    bl_label = "Unpin Object"
+    bl_idname = "space.unpin_object"
+    bl_options = {'INTERNAL'}
 
-    if space.use_pin_id:
-        row = layout.row()
-        row.prop_search(context.scene, "physics_pin_object", context.scene, "physics_pin_object_list", text='', icon='OBJECT_DATA')
-        row.operator("space.pin_physics_object", text="", icon="UNPINNED")
+    @classmethod
+    def poll(cls, context):
+        return context.space_data.type == 'PROPERTIES' and context.space_data.use_pin_id
+
+    def execute(self, context):
+        context.space_data.use_pin_id = False
+        return {'FINISHED'}
 
 def has_physics(obj):
     if len(obj.particle_systems) > 0:
@@ -58,18 +63,40 @@ def upd_handler(scene):
                 pinstr = ob.name
     scene.physics_pin_object = pinstr
 
+class PHYSICS_PT_pin_physics_object(bpy.types.Panel):
+    bl_label = ""
+    bl_options = {'HIDE_HEADER'}
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "physics"
+
+    def draw(self, context):
+        layout = self.layout
+        space = context.space_data
+
+        layout.label("Objects with caches:")
+        row = layout.row()
+        row.prop_search(context.scene, "physics_pin_object", context.scene, "physics_pin_object_list", text='', icon='OBJECT_DATA')
+        if space.use_pin_id and space.pin_id.name == context.scene.physics_pin_object:
+            #row.operator(SPACE_OT_unpin_object.bl_idname, text="", icon="PINNED", emboss=False)
+            row.operator(SPACE_OT_pin_physics_object.bl_idname, text="", icon="PINNED", emboss=False)
+        else:
+            row.operator(SPACE_OT_pin_physics_object.bl_idname, text="", icon="UNPINNED", emboss=False)
+
 def register():
     bpy.utils.register_class(SPACE_OT_pin_physics_object)
+    #bpy.utils.register_class(SPACE_OT_unpin_object)
+    bpy.utils.register_class(PHYSICS_PT_pin_physics_object)
     bpy.types.Scene.physics_pin_object = bpy.props.StringProperty(options={'HIDDEN','SKIP_SAVE'})
     bpy.types.Scene.physics_pin_object_list = bpy.props.CollectionProperty(type=bpy.types.PropertyGroup, options={'HIDDEN','SKIP_SAVE'})
-    bpy.types.PHYSICS_PT_add.prepend(draw_pin_selector)
     bpy.app.handlers.scene_update_post.append(upd_handler)
 
 def unregister():
     bpy.utils.unregister_class(SPACE_OT_pin_physics_object)
+    #bpy.utils.unregister_class(SPACE_OT_unpin_object)
+    bpy.utils.unregister_class(PHYSICS_PT_pin_physics_object)
     del bpy.types.Scene.physics_pin_object
     del bpy.types.Scene.physics_pin_object_list
-    bpy.types.PHYSICS_PT_add.remove(draw_pin_selector)
     bpy.app.handlers.scene_update_post.remove(upd_handler)
 
 if __name__ == '__main__':
